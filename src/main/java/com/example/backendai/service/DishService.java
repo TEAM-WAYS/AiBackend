ab683736262438f9ce6be33fa07fc4799d4fddee
Andsemandse <andr90m3@stud.kea.dk>
2023-11-06 12:52:20 +0100

    @service fejl er fixet ! ! !

--- src/main/java/com/example/backendai/service/DishService.java
+++ src/main/java/com/example/backendai/service/DishService.java
@@ -1,10 +1,12 @@
 package com.example.backendai.service;
 
 import com.example.backendai.model.Dish;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public interface DishService {
     Optional<Dish> putDish(Dish dish);
 
