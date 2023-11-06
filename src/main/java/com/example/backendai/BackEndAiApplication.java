9d9cddc6f126d2da5c2b562cb62a088b077a0d24
Andsemandse <andr90m3@stud.kea.dk>
2023-11-06 09:32:35 +0100

    start

---
+++ src/main/java/com/example/backendai/BackEndAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendai/BackEndAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
