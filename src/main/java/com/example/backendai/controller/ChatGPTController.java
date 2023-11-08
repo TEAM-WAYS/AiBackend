58561ef109f88c76a9f191805ea9e581285fec54
AmingYee <wenm0007@stud.kea.dk>
2023-11-08 02:26:10 +0100

    backend ai fixed

--- src/main/java/com/example/backendai/controller/ChatGPTController.java
+++ src/main/java/com/example/backendai/controller/ChatGPTController.java
@@ -1,35 +1,35 @@
-package com.example.backendai.service;
+package com.example.backendai.controller;
 
-
+import com.example.backendai.dto.ChatRequest;
 import com.example.backendai.dto.ChatResponse;
 import com.example.backendai.dto.Choice;
 import com.example.backendai.dto.Message;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Service;
-
-import com.example.backendai.dto.ChatRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+public class ChatGPTController {
 
-@Service
-public class ChatGtpDialog {
-
-    private WebClient webClient;
+    private final WebClient webClient;
 
 
-    public ChatGtpDialog(WebClient.Builder webClientBuilder) {
+    public ChatGPTController(WebClient.Builder webClientBuilder) {
         this.webClient = webClientBuilder.baseUrl("https://api.openai.com/v1/chat/completions").build();
     }
 
-    public List<Choice> chatWithGPT(String question) {
+    @GetMapping("/chat")
+    public List<Choice> chatWithGPT(@RequestParam String message) {
         ChatRequest chatRequest = new ChatRequest();
         chatRequest.setModel("gpt-3.5-turbo");
         List<Message> lstMessages = new ArrayList<>(); //en liste af messages med roller
         lstMessages.add(new Message("system", "You are a helpful assistant."));
-        lstMessages.add(new Message("user", "Where is Copenhagen"));
+        lstMessages.add(new Message("user", message));
         chatRequest.setMessages(lstMessages);
         chatRequest.setN(1); //n er antal svar fra chatgpt
         chatRequest.setTemperature(1); //jo højere jo mere fantasifuldt svar (se powerpoint)
@@ -39,7 +39,7 @@ public class ChatGtpDialog {
 
         ChatResponse response = webClient.post()
                 .contentType(MediaType.APPLICATION_JSON)
-                .headers(h -> h.setBearerAuth("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"))
+                .headers(h -> h.setBearerAuth("insert api key"))
                 .bodyValue(chatRequest)
                 .retrieve()
                 .bodyToMono(ChatResponse.class)
@@ -49,15 +49,4 @@ public class ChatGtpDialog {
 
         return lst;
     }
-
-
-
-
-
-
-
-
-
-
-
 }
