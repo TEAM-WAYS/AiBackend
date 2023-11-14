b1ff0f302df1708227edecfd89dd3fe7baa8db87
yuyi0011 <yuyi0011@stud.kea.dk>
2023-11-14 11:32:11 +0100

    UserController updated

--- src/main/java/com/example/backendai/controller/UserController.java
+++ src/main/java/com/example/backendai/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.backendai.controller;
+import com.example.backendai.model.Recipe;
 import com.example.backendai.model.User;
 import com.example.backendai.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,15 +12,22 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
-public class userController {
+public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping("users")
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
+        public ResponseEntity<List<User>> getAllUser() {
+        List<User> userList = userRepository.findAll();
+        //  return userRepository.findAll();
+            if (userList.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(userList);
+            } else {
+                return ResponseEntity.status(HttpStatus.OK).body(userList);
+            }
+        }
+
     @GetMapping("/{id}")
     public Optional<User> getUserById(@PathVariable int id) {
         return userRepository.findById(id);
